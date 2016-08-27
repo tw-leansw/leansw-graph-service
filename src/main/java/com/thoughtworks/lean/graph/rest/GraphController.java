1c2ceebfbd568cc2e4748fc0ff51e28607508198
YongliuLi <ylli@thoughtworks.com>
2016-08-27 13:09:10 +0800

    [#0] add .kibana/index-pattern/_search and .kibana/_mapping/*/field/_source api

--- src/main/java/com/thoughtworks/lean/graph/rest/GraphController.java
+++ src/main/java/com/thoughtworks/lean/graph/rest/GraphController.java
@@ -6,14 +6,12 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Collections;
-
-/**
- * Created by yongliuli on 8/26/16.
- */
 @RequestMapping(path = {"/api/graph"})
 @RestController
 public class GraphController {
@@ -36,13 +34,27 @@ public class GraphController {
     @Value("${kibana.uri}")
     String kibanaUri = "http://deliflow-kibana-server:8088/";
 
-    @RequestMapping(value = "get")
+    @RequestMapping(value = "mget")
     public String mGet(@RequestBody String req) {
         return restTemplate.postForObject(kibanaUri + "elasticsearch/_mget?timeout=0&ignore_unavailable=true", getRequest(req), String.class);
     }
 
-    @RequestMapping(value = "search")
+    @RequestMapping(value = "msearch")
     public String mSearch(@RequestBody String req) {
         return restTemplate.postForObject(kibanaUri + "elasticsearch/_msearch?timeout=0&ignore_unavailable=true", getRequest(req), String.class);
     }
+
+    @RequestMapping(value = "index-pattern/search")
+    public String indexPatternSearch(@RequestBody String req, @RequestParam("fields") String[] fields) {
+        String url = UriComponentsBuilder.fromHttpUrl(kibanaUri + "elasticsearch/.kibana/index-pattern/_search").queryParam("fields", fields).build().toUriString();
+        return restTemplate.postForObject(url, getRequest(req), String.class);
+    }
+
+    @RequestMapping(value = "mapping/field/source")
+    public String mappingFieldSource(@RequestBody String req, @RequestParam("_") String _) {
+        String url = UriComponentsBuilder.fromHttpUrl(kibanaUri + "elasticsearch/.kibana/_mapping/*/field/_source").queryParam("_", _).build().toUriString();
+        return restTemplate.postForObject(url, getRequest(req), String.class);
+    }
+
+
 }
